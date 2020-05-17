039b7da3b7b4b53c01dc4e7c44b5d9002dfa1a64
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2020-05-17 12:29:10 +0200

    sliders mixers

--- Beer Pong AR/Assets/Scripts/MainMenuFuncs.cs
+++ Beer Pong AR/Assets/Scripts/MainMenuFuncs.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class MainMenuFuncs : MonoBehaviour
@@ -10,14 +9,19 @@ public class MainMenuFuncs : MonoBehaviour
     // Start is called before the first frame update
     public GameObject panelSettings;
     public GameObject mainPanel;
-    public Slider musicSlider;
     public AudioMixer audio_mixer;
     public AudioMixer sfx_mixer;
-    private float volume;
+   
+
     void Start()
     {
  
 
+    }
+
+    void Update()
+    {
+      
     }
 
     public void PlayGame()
@@ -42,11 +46,17 @@ public class MainMenuFuncs : MonoBehaviour
     public void SetMusicValue(float sliderValue)
     {
         audio_mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        //float db;
+        //audio_mixer.GetFloat("MusicVolume", out db);
+        //PlayerPrefs.SetFloat("MusicVolume", db);
     }
 
     public void SetSFXValue(float sliderValue)
     {
         sfx_mixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
+        //float db;
+        //sfx_mixer.GetFloat("SFXVolume", out db);
+        //PlayerPrefs.SetFloat("SFXVolume", db);
     }
 
     public void ExitGame()
--- Beer Pong AR/Assets/Scripts/SettingsInGame.cs
+++ Beer Pong AR/Assets/Scripts/SettingsInGame.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsInGame : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject settings_panel;
-    public AudioClip open_menu;
-    AudioSource audio_source;
+    public AudioSource pauseButtonAudioSource;
+    public AudioMixer audio_mixer;
+    public AudioMixer sfx_mixer;
+    public Slider sliderMusic;
+    public Slider sliderSFX;
+    private float dBMusic;
+    private float dBSFX;
+
 
     void Start()
     {
-        audio_source = GetComponent<AudioSource>();
+        audio_mixer.GetFloat("MusicVolume", out dBMusic);
+        sliderMusic.value = Mathf.Pow(10, dBMusic / 20);
+        sfx_mixer.GetFloat("SFXVolume", out dBSFX);
+        sliderSFX.value = Mathf.Pow(10, dBSFX / 20);
+
+        
         settings_panel.SetActive(false);
     }
 
@@ -26,18 +39,14 @@ public class SettingsInGame : MonoBehaviour
     {
         if (!settings_panel.active)
         {
-            audio_source.Stop();
-            audio_source.clip = open_menu;
-            audio_source.Play();
+            pauseButtonAudioSource.Play();
 
             settings_panel.SetActive(true);
             Time.timeScale = 0.0f;
         }
         else
         {
-            audio_source.Stop();
-            audio_source.clip = open_menu;
-            audio_source.Play();
+            pauseButtonAudioSource.Play();
 
             Time.timeScale = 1.0f;
             settings_panel.SetActive(false);
@@ -48,5 +57,18 @@ public class SettingsInGame : MonoBehaviour
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void SetMusicValue(float sliderValue)
+    {
+        audio_mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+  
+    }
+
+    public void SetSFXValue(float sliderValue)
+    {
+        sfx_mixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
+       
     }
 }
